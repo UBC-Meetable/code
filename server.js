2924cf81841467f8776ac8fa83cf7a37c16d6087
pasitew <68790666+pasitew@users.noreply.github.com>
2020-07-25 16:54:52 -0400

    my changes

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 
 const app = express()
-
+app.get('/xyz', (req, res)=>{
+    res.send("Hi")
+})
 
 app.listen(3000, ()=> console.log("Meetable api listening on port 3000"));
