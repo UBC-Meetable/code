38c695f1af057c6a70c057b6e7ec61933e5d3048
AlexZhou876 <69568440+AlexZhou876@users.noreply.github.com>
2021-01-06 00:06:27 -0800

    Revert "moved everything inside meetable-api"

--- meetable-api/models/Image.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const ImageSchema = new mongoose.Schema({
-  img: {
-    data: Buffer,
-    contentType: String,
-  },
-});
-module.exports = Image = mongoose.model("image", ImageSchema);
