04eee153c1c5fb22200a33bf973aabe69075f397
moseskirathe <moskirathe@gmail.com>
2021-01-24 15:25:08 -0800

    quiz screen. Pending buttons and styling

---
+++ meetable-app/data/data.tsx
@@ -0,0 +1,267 @@
+export default [
+  {
+    "title": "Art?",
+    "id": "1",
+    "img": require('../assets/quiz_images/Art.png'),
+  },
+  {
+    "title": "Biking?",
+    "id": "2",
+    "img": require('../assets/quiz_images/Biking.png'),
+  },
+  {
+    "title": "Fitness?",
+    "id": "3",
+    "img": require('../assets/quiz_images/Fitness.png'),
+  },
+  {
+    "title": "Pizza?",
+    "id": "4",
+    "img": require('../assets/quiz_images/Pizza.png'),
+  },
+  {
+    "title": "Coding?",
+    "id": "5",
+    "img": require('../assets/quiz_images/Coding.png'),
+  },
+  {
+    "title": "Drinking?",
+    "id": "6",
+    "img": require('../assets/quiz_images/Drinking.png'),
+  },
+  {
+    "title": "Instruments?",
+    "id": "7",
+    "img": require('../assets/quiz_images/Instruments.png'),
+  },
+  {
+    "title": "Learning Languages?",
+    "id": "8",
+    "img": require('../assets/quiz_images/LearningLanguages.png'),
+  },
+  {
+    "title": "Camping?",
+    "id": "9",
+    "img": require('../assets/quiz_images/Camping.png'),
+  },
+  {
+    "title": "Texting?",
+    "id": "10",
+    "img": require('../assets/quiz_images/Texting.png'),
+  },
+  {
+    "title": "Studying with Friends?",
+    "id": "11",
+    "img": require('../assets/quiz_images/StudyingWithFriends.png'),
+  },
+  {
+    "title": "Pop Music?",
+    "id": "12",
+    "img": require('../assets/quiz_images/PopMusic.png'),
+  },
+  {
+    "title": "Zoom Hangouts?",
+    "id": "13",
+    "img": require('../assets/quiz_images/ZoomHangouts.png'),
+  },
+  {
+    "title": "Yoga?",
+    "id": "14",
+    "img": require('../assets/quiz_images/Yoga.png'),
+  },
+  {
+    "title": "Tech?",
+    "id": "15",
+    "img": require('../assets/quiz_images/Tech.png'),
+  },
+  {
+    "title": "Tennis?",
+    "id": "16",
+    "img": require('../assets/quiz_images/Tennis.png'),
+  },
+  {
+    "title": "Science?",
+    "id": "18",
+    "img": require('../assets/quiz_images/Science.png'),
+  },
+  {
+    "title": "Math?",
+    "id": "19",
+    "img": require('../assets/quiz_images/Math.png'),
+  },
+  {
+    "title": "Marvel Superheroes?",
+    "id": "20",
+    "img": require('../assets/quiz_images/Marvel.png'),
+  },
+  {
+    "title": "Hockey?",
+    "id": "21",
+    "img": require('../assets/quiz_images/Hockey.png'),
+  },
+  {
+    "title": "Running?",
+    "id": "22",
+    "img": require('../assets/quiz_images/Running.png'),
+  },
+  {
+    "title": "Deep Talk?",
+    "id": "23",
+    "img": require('../assets/quiz_images/DeepTalk.png'),
+  },
+  {
+    "title": "Horror Movies?",
+    "id": "24",
+    "img": require('../assets/quiz_images/HorrorMovies.png'),
+  },
+  {
+    "title": "Partying?",
+    "id": "25",
+    "img": require('../assets/quiz_images/Partying.png'),
+  },
+  {
+    "title": "Photography?",
+    "id": "26",
+    "img": require('../assets/quiz_images/Photography.png'),
+  },
+  {
+    "title": "Pets?",
+    "id": "27",
+    "img": require('../assets/quiz_images/Pets.png'),
+  },
+  {
+    "title": "Reading?",
+    "id": "28",
+    "img": require('../assets/quiz_images/Reading.png'),
+  },
+  {
+    "title": "Outdoors?",
+    "id": "29",
+    "img": require('../assets/quiz_images/Outdoors.png'),
+  },
+  {
+    "title": "Fashion?",
+    "id": "30",
+    "img": require('../assets/quiz_images/Fashion.png'),
+  },
+  {
+    "title": "Singing?",
+    "id": "31",
+    "img": require('../assets/quiz_images/Singing.png'),
+  },
+  {
+    "title": "Travel?",
+    "id": "32",
+    "img": require('../assets/quiz_images/Travel.png'),
+  },
+  {
+    "title": "Among Us?",
+    "id": "33",
+    "img": require('../assets/quiz_images/AmongUs.png'),
+  },
+  {
+    "title": "Basketball?",
+    "id": "34",
+    "img": require('../assets/quiz_images/Basketball.png'),
+  },
+  {
+    "title": "Baseball?",
+    "id": "35",
+    "img": require('../assets/quiz_images/Baseball.png'),
+  },
+  {
+    "title": "Business",
+    "id": "36",
+    "img": require('../assets/quiz_images/Business.png'),
+  },
+  {
+    "title": "Cats?",
+    "id": "37",
+    "img": require('../assets/quiz_images/Cats.png'),
+  },
+  {
+    "title": "Comedies?",
+    "id": "38",
+    "img": require('../assets/quiz_images/Comedies.png'),
+  },
+  {
+    "title": "Meditation?",
+    "id": "39",
+    "img": require('../assets/quiz_images/Meditation.png'),
+  },
+  {
+    "title": "Cooking?",
+    "id": "40",
+    "img": require('../assets/quiz_images/Cooking.png'),
+  },
+  {
+    "title": "Dancing?",
+    "id": "41",
+    "img": require('../assets/quiz_images/Dancing.png'),
+  },
+  {
+    "title": "Dogs?",
+    "id": "42",
+    "img": require('../assets/quiz_images/Dogs.png'),
+  },
+  {
+    "title": "Hiking?",
+    "id": "43",
+    "img": require('../assets/quiz_images/Hiking.png'),
+  },
+  {
+    "title": "Hip Hop Music",
+    "id": "44",
+    "img": require('../assets/quiz_images/HipHopMusic.png'),
+  },
+  {
+    "title": "Martial Arts",
+    "id": "45",
+    "img": require('../assets/quiz_images/MartialArts.png'),
+  },
+  {
+    "title": "Philosophy",
+    "id": "46",
+    "img": require('../assets/quiz_images/Philosophy.png'),
+  },
+  {
+    "title": "Studying",
+    "id": "47",
+    "img": require('../assets/quiz_images/Studying.png'),
+  },
+  {
+    "title": "Rock Music",
+    "id": "48",
+    "img": require('../assets/quiz_images/RockMusic.png'),
+  },
+  {
+    "title": "Small Talk",
+    "id": "49",
+    "img": require('../assets/quiz_images/SmallTalk.png'),
+  },
+  {
+    "title": "Soccer?",
+    "id": "50",
+    "img": require('../assets/quiz_images/Soccer.png'),
+  },
+  {
+    "title": "Tv Shows?",
+    "id": "51",
+    "img": require('../assets/quiz_images/TVShows.png'),
+  },
+  {
+    "title": "Video Games?",
+    "id": "52",
+    "img": require('../assets/quiz_images/VideoGames.png'),
+  },
+  {
+    "title": "Volleyball?",
+    "id": "53",
+    "img": require('../assets/quiz_images/Volleyball.png'),
+  },
+  {
+    "title": "Zoom Calling?",
+    "id": "54",
+    "img": require('../assets/quiz_images/ZoomCalling.png'),
+  },
+]
--- meetable-app/screens/QuizScreen.tsx
+++ meetable-app/screens/QuizScreen.tsx
@@ -1,119 +1,103 @@
-import { StackNavigationProp } from "@react-navigation/stack";
-import { Button, Layout, Text } from "@ui-kitten/components";
-import React from "react";
-import { Dimensions, StyleSheet } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import React, {Component} from 'react';
+import { StyleSheet, Text, View, 
+  Image, 
+  SafeAreaView,
+  Dimensions} from 'react-native';
+import Swipe from '../components/Swipe';
+import questions from '../data/data';
+import { Card, Button } from 'react-native-elements';
+import { DeviceEventEmitter } from 'react-native';
+import { QuestionType } from '../types';
 import BubbleBackground from "../assets/images/tutorial-bubble.svg";
-import { RootStackParamList } from "../types";
+
 
 const window = Dimensions.get("window");
+class QuizScreen extends Component {
+  quizState = {
+    id: null,
+    responses: Array<QuestionType>()
+  };
 
-const QuizScreen = ({
-  navigation,
-}: {
-  navigation: StackNavigationProp<RootStackParamList, "Quiz">;
-}) => (
-  <Layout style={styles.root}>
-    <BubbleBackground
-      width={window.width}
-      height={window.height}
-      style={{ position: "absolute" }}
-    />
+  handleResponse = (question: QuestionType, response: string) => {
+    question.response = response;
+    this.quizState.responses.push(question);
+  }
 
-    <Layout style={styles.bottomcontainer}>
-      <Button
-        style={styles.button}
-        onPress={() => {
-          navigation.replace("Signup");
-        }}
-      >
-        {(evaProps: any) => (
-          <Text
-            {...evaProps}
-            style={{ ...evaProps.style, ...styles.buttonText }}
-          >
-            End Quiz
-          </Text>
-        )}
-      </Button>
-    </Layout>
-  </Layout>
-);
+
+  renderCards= (question: QuestionType, i:number) => {
+    return (
+      <Card>
+        <View style={styles.card}>
+        <Text style={styles.questionTitle}>{question.title}</Text>
+        <View style={styles.questionView}>
+          <Image
+            source={question.img}
+            style={{ width: '100%', height: 600 }}
+          />
+        </View>
+        </View>
+      </Card>
+    );
+  };
+
+  renderNoMoreCards = () => {
+    return (
+      <Card>
+        <Text>No more cards to render. Call move to auth0 screen fn</Text>
+      </Card>
+    );
+  };
+
+  render() {
+  return (
+    <SafeAreaView style={styles.container}>
+        {/* <BubbleBackground width={window.width} height={window.height} style={{ position: "absolute" }} /> */}
+        <Swipe 
+        onSwipe = {this.handleResponse}
+        data={questions} 
+        keyProp="id"
+        renderCard={this.renderCards}
+        renderNoMoreCards={this.renderNoMoreCards}/>
+      </SafeAreaView>
+  );
+}
+}
 
 const styles = StyleSheet.create({
-  title: {
-    color: "#FBBA82",
-  },
-  root: {
+  container: {
     flex: 1,
-    display: "flex",
-    height: "100%",
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#FFF8F3",
+    backgroundColor: '#FFF8F3',
   },
   card: {
-    width: "100%",
-    padding: 0,
-    margin: 0,
-    height: "100%",
-    maxHeight: "91%",
-    backgroundColor: "#0000",
-    borderWidth: 0,
+    backgroundColor: '#FFF8F3', // CHANGE TO TRANSPARENT
+    marginTop:-20,
+    marginBottom:-20,
+    marginRight:-20,
+    marginLeft:-20,
   },
-  button: {
-    marginBottom: 20,
-    width: "90%",
-    borderRadius: 100,
-    borderWidth: 0,
-    backgroundColor: "#02A3F4",
+  questionView: {
+    alignContent: 'center',
+    backgroundColor: "#F9DAC4",
+    borderRadius: 30,
   },
-  buttonText: {
-    fontSize: 20,
-    textAlign: "center",
-    flex: 1,
-  },
-  carousel: {
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "flex-end",
-  },
-  blacktext: {
-    color: "#000",
-  },
-  slide: {
-    height: "90%",
-    display: "flex",
-    width: "90%",
-    marginTop: 10,
-    backgroundColor: "rgba(0,0,0,0)",
-    justifyContent: "center",
-    alignItems: "center",
-    color: "#000000",
-  },
-  topcontainer: {
-    flex: 1,
-    backgroundColor: "rgba(0,0,0,0)",
-    width: "100%",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  bottomcontainer: {
-    flex: 0,
-    display: "flex",
-    flexBasis: 150,
-    flexDirection: "column",
-    alignItems: "center",
-    backgroundColor: "#0000",
-  },
-  carouselContainer: {
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
-    height: "100%",
+  questionTitle: {
+    color: '#FBBA82',
+    fontSize: 30,
+    fontWeight: 'bold',
+    textAlign: 'center',
     marginTop: 30,
+    marginBottom: 30,
   },
+  buttonsContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonContainer: {
+    flex: 1,
+  }
 });
 
 export default QuizScreen;
