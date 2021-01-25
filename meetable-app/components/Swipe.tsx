01c8fbff0cc178d83a227b9c31f67ef3ba2ed759
moseskirathe <moskirathe@gmail.com>
2021-01-24 19:48:53 -0800

    lint fix

--- meetable-app/components/Swipe.tsx
+++ meetable-app/components/Swipe.tsx
@@ -1,22 +1,23 @@
-/* eslint-disable no-unused-expressions */
 /* eslint-disable react/no-access-state-in-setstate */
-
-import React, { Component } from 'react';
-import { StyleSheet, DeviceEventEmitter, PanResponderInstance } from 'react-native';
+/* eslint-disable react/destructuring-assignment */
+import React, { Component } from "react";
 import {
-  View, PanResponder, Animated, Dimensions
-} from 'react-native';
-import { QuestionType } from '../types';
+  StyleSheet, PanResponderInstance,
+  View, PanResponder, Animated, Dimensions,
+} from "react-native";
 
-const SCREEN_WIDTH = Dimensions.get('window').width;
+import { QuestionType } from "../types";
+
+const SCREEN_WIDTH = Dimensions.get("window").width;
 const SWIPE_THRESHOLD = 0.25 * SCREEN_WIDTH;
 const SWIPE_OUT_DURATION = 250;
 type SwipeProps = {
+    // eslint-disable-next-line no-unused-vars
     onSwipe: (question: QuestionType, response: string)=>void,
     data: QuestionType[],
     keyProp: string,
-    renderCard: Function, 
-    renderNoMoreCards: Function, 
+    renderCard: Function,
+    renderNoMoreCards: Function,
 }
 type SwipeState = {
     position: any,
@@ -24,111 +25,114 @@ type SwipeState = {
     index: number,
 }
 class Swipe extends Component<SwipeProps, SwipeState> {
-    constructor(props: SwipeProps) {
-        super(props);
-        this.state = {
-            position: new Animated.ValueXY(),
-            panResponder: PanResponder.create({
-                // Ask to be the responder:
-                onStartShouldSetPanResponder: (evt, gestureState) => true,
-                onPanResponderMove: (evt, gestureState) => {
-                  // The most recent move distance is gestureState.move{X,Y}
-                  // The accumulated gesture distance since becoming responder is gestureState.d{x,y}
-                  this.state.position.setValue({x: gestureState.dx, y: gestureState.dy});
-                },
-                onPanResponderRelease: (evt, gestureState) => {
-                  // The user has released all touches while this view is the
-                  // responder. This typically means a gesture has succeeded
-                  if (gestureState.dx > SWIPE_THRESHOLD) {
-                      this.forceSwipe('right'); // User swiped right
-                    } else if (gestureState.dx < -SWIPE_THRESHOLD) {
-                      this.forceSwipe('left'); // User swiped left
-                    } else {
-                      this.resetPosition();
-                    }
-                },
-              }),
-              index: 0,
-        }
-      }
-
-    onSwipeComplete(direction: string, btnName = null) {
-        const { onSwipe, data } = this.props;
-        const item = data[this.state.index];
-        direction === 'right' ? onSwipe(item, "liked") : onSwipe(item, "disliked");
-        this.state.position.setValue({ x: 0, y: 0 });
-        this.setState({ index: this.state.index + 1 });
-      }
-
-      getCardStyle() {
-        const { position } = this.state;
-        const rotate = position.x.interpolate({
-          inputRange: [-SCREEN_WIDTH * 1.5, 0, SCREEN_WIDTH * 1.5],
-          outputRange: ['-120deg', '0deg', '120deg']
-        });
-    
-        return {
-          ...position.getLayout(),
-          transform: [{ rotate }]
-        };
-      }
-
-      forceSwipe(direction: string) {
-        const x = direction === 'right' ? SCREEN_WIDTH : -SCREEN_WIDTH;
-        Animated.timing(this.state.position, {
-          toValue: { x, y: 0 },
-          useNativeDriver: false,
-          duration: SWIPE_OUT_DURATION
-        }).start(() => this.onSwipeComplete(direction));
-      }
-      
-      resetPosition() {
-        Animated.spring(this.state.position, {
-          toValue: { x: 0, y: 0 },
-          useNativeDriver: false
-        }).start();
-      }
-    renderCards = () => {
-        if (this.state.index >= this.props.data.length) {
-          return this.props.renderNoMoreCards();
-        }
-    
-        return this.props.data.map((item, i) => {
-          if (i < this.state.index) { return null; }
-    
-          if (i === this.state.index) {
-            return (
-              <Animated.View
-                key={item.id}
-                style={[this.getCardStyle(), styles.cardStyle]}
-                {...this.state.panResponder.panHandlers}
-              >
-                {this.props.renderCard(item)}
-              </Animated.View>
-            );
+  constructor(props: SwipeProps) {
+    super(props);
+    this.state = {
+      position: new Animated.ValueXY(),
+      panResponder: PanResponder.create({
+        // Ask to be the responder:
+        onStartShouldSetPanResponder: () => true,
+        onPanResponderMove: (evt, gestureState) => {
+          // The most recent move distance is gestureState.move{X,Y}
+          // The accumulated gesture distance since becoming responder is gestureState.d{x,y}
+          // eslint-disable-next-line react/destructuring-assignment
+          this.state.position.setValue({ x: gestureState.dx, y: gestureState.dy });
+        },
+        onPanResponderRelease: (evt, gestureState) => {
+          // The user has released all touches while this view is the
+          // responder. This typically means a gesture has succeeded
+          if (gestureState.dx > SWIPE_THRESHOLD) {
+            this.forceSwipe("right"); // User swiped right
+          } else if (gestureState.dx < -SWIPE_THRESHOLD) {
+            this.forceSwipe("left"); // User swiped left
+          } else {
+            this.resetPosition();
           }
-    
+        },
+      }),
+      index: 0,
+    };
+  }
+
+  onSwipeComplete(direction: string) {
+    const { onSwipe, data } = this.props;
+    const item = data[this.state.index];
+    // eslint-disable-next-line no-unused-expressions
+    direction === "right" ? onSwipe(item, "liked") : onSwipe(item, "disliked");
+    this.state.position.setValue({ x: 0, y: 0 });
+    this.setState({ index: this.state.index + 1 });
+  }
+
+  getCardStyle() {
+    const { position } = this.state;
+    const rotate = position.x.interpolate({
+      inputRange: [-SCREEN_WIDTH * 1.5, 0, SCREEN_WIDTH * 1.5],
+      outputRange: ["-120deg", "0deg", "120deg"],
+    });
+
+    return {
+      ...position.getLayout(),
+      transform: [{ rotate }],
+    };
+  }
+
+  forceSwipe(direction: string) {
+    const x = direction === "right" ? SCREEN_WIDTH : -SCREEN_WIDTH;
+    Animated.timing(this.state.position, {
+      toValue: { x, y: 0 },
+      useNativeDriver: false,
+      duration: SWIPE_OUT_DURATION,
+    }).start(() => this.onSwipeComplete(direction));
+  }
+
+  resetPosition() {
+    Animated.spring(this.state.position, {
+      toValue: { x: 0, y: 0 },
+      useNativeDriver: false,
+    }).start();
+  }
+
+    renderCards = () => {
+      if (this.state.index >= this.props.data.length) {
+        return this.props.renderNoMoreCards();
+      }
+
+      return this.props.data.map((item, i) => {
+        if (i < this.state.index) { return null; }
+
+        if (i === this.state.index) {
           return (
             <Animated.View
               key={item.id}
-              style={[styles.cardStyle]}
+              style={[this.getCardStyle(), styles.cardStyle]}
+              {...this.state.panResponder.panHandlers}
             >
               {this.props.renderCard(item)}
             </Animated.View>
           );
-        }).reverse();
-    
-     };
-  render() {
-    return <View>{this.renderCards()}</View>;
-  }
+        }
+
+        return (
+          <Animated.View
+            key={item.id}
+            style={[styles.cardStyle]}
+          >
+            {this.props.renderCard(item)}
+          </Animated.View>
+        );
+      }).reverse();
+    };
+
+    render() {
+      return <View>{this.renderCards()}</View>;
+    }
 }
 
 const styles = StyleSheet.create({
-    cardStyle: {
-        position: 'absolute',
-        width: SCREEN_WIDTH,
-      }
-  });
+  cardStyle: {
+    position: "absolute",
+    width: SCREEN_WIDTH,
+  },
+});
 
 export default Swipe;
--- meetable-app/screens/QuizScreen.tsx
+++ meetable-app/screens/QuizScreen.tsx
@@ -1,22 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import React, {Component} from 'react';
-import { StyleSheet, Text, View, 
-  Image, 
+/* eslint-disable no-array-constructor */
+/* eslint-disable no-param-reassign */
+import React, { Component } from "react";
+import {
+  StyleSheet, Text, View,
+  Image,
   SafeAreaView,
-  Dimensions} from 'react-native';
-import Swipe from '../components/Swipe';
-import questions from '../data/data';
-import { Card, Button } from 'react-native-elements';
-import { DeviceEventEmitter } from 'react-native';
-import { QuestionType } from '../types';
-import BubbleBackground from "../assets/images/tutorial-bubble.svg";
+} from "react-native";
+import { Card } from "react-native-elements";
+import Swipe from "../components/Swipe";
+import questions from "../data/data";
+import { QuestionType } from "../types";
 
-
-const window = Dimensions.get("window");
 class QuizScreen extends Component {
   quizState = {
     id: null,
-    responses: Array<QuestionType>()
+    responses: Array<QuestionType>(),
   };
 
   handleResponse = (question: QuestionType, response: string) => {
@@ -24,80 +22,79 @@ class QuizScreen extends Component {
     this.quizState.responses.push(question);
   }
 
-
-  renderCards= (question: QuestionType, i:number) => {
-    return (
-      <Card>
-        <View style={styles.card}>
+  renderCards= (question: QuestionType) => (
+    <Card>
+      <View style={styles.card}>
         <Text style={styles.questionTitle}>{question.title}</Text>
         <View style={styles.questionView}>
           <Image
             source={question.img}
-            style={{ width: '100%', height: 600 }}
+            style={{ width: "100%", height: 600 }}
           />
         </View>
-        </View>
-      </Card>
-    );
-  };
+      </View>
+    </Card>
+  );
 
-  renderNoMoreCards = () => {
-    return (
-      <Card>
-        <Text>No more cards to render. Call move to auth0 screen fn</Text>
-      </Card>
-    );
-  };
+  renderNoMoreCards = () => (
+    <Card>
+      <Text>No more cards to render. Call move to auth0 screen fn</Text>
+    </Card>
+  );
 
   render() {
-  return (
-    <SafeAreaView style={styles.container}>
-        {/* <BubbleBackground width={window.width} height={window.height} style={{ position: "absolute" }} /> */}
-        <Swipe 
-        onSwipe = {this.handleResponse}
-        data={questions} 
-        keyProp="id"
-        renderCard={this.renderCards}
-        renderNoMoreCards={this.renderNoMoreCards}/>
+    return (
+      <SafeAreaView style={styles.container}>
+        {/* <BubbleBackground
+        width={window.width}
+        height={window.height}
+        style={{ position: "absolute" }} /> */}
+        <Swipe
+          onSwipe={this.handleResponse}
+          data={questions}
+          keyProp="id"
+          renderCard={this.renderCards}
+          renderNoMoreCards={this.renderNoMoreCards}
+        />
       </SafeAreaView>
-  );
-}
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#FFF8F3',
+    backgroundColor: "#FFF8F3",
   },
   card: {
-    backgroundColor: '#FFF8F3', // CHANGE TO TRANSPARENT
-    marginTop:-20,
-    marginBottom:-20,
-    marginRight:-20,
-    marginLeft:-20,
+    backgroundColor: "#FFF8F3", // CHANGE TO TRANSPARENT
+    marginTop: -20,
+    marginBottom: -20,
+    marginRight: -20,
+    marginLeft: -20,
   },
   questionView: {
-    alignContent: 'center',
+    alignContent: "center",
     backgroundColor: "#F9DAC4",
     borderRadius: 30,
   },
   questionTitle: {
-    color: '#FBBA82',
+    color: "#FBBA82",
     fontSize: 30,
-    fontWeight: 'bold',
-    textAlign: 'center',
+    fontWeight: "bold",
+    textAlign: "center",
     marginTop: 30,
     marginBottom: 30,
   },
   buttonsContainer: {
     flex: 1,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
   },
   buttonContainer: {
     flex: 1,
-  }
+  },
 });
 
 export default QuizScreen;
