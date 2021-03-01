589dc0595ef5eb531e02f2f34c11b217e26540e7
moseskirathe <moskirathe@gmail.com>
2021-02-28 19:48:41 -0800

    quiz buttons functionality

--- meetable-app/screens/QuizScreen.tsx
+++ meetable-app/screens/QuizScreen.tsx
@@ -2,12 +2,11 @@
 /* eslint-disable no-param-reassign */
 import React, { Component } from "react";
 import {
-  StyleSheet, Text, View,
-  Image,
+  StyleSheet, Text,
   SafeAreaView,
   Dimensions,
 } from "react-native";
-import { Card, Icon } from "react-native-elements";
+import { Card } from "react-native-elements";
 import Swipe from "../components/Swipe";
 import questions from "../data/data";
 import { QuestionType } from "../types";
@@ -24,58 +23,6 @@ class QuizScreen extends Component {
     question.response = response;
     this.quizState.responses.push(question);
   }
-
-  handleButtonPress = () => {
-
-  }
-
-  renderCards= (question: QuestionType) => (
-    <View>
-      <View style={styles.card}>
-        <Text style={styles.questionTitle}>{question.title}</Text>
-        <View style={styles.questionView}>
-          <Image
-            source={question.img}
-            style={{ width: "100%", height: "100%" }}
-          />
-        </View>
-        <View style={styles.buttonsContainer}>
-          <Icon
-            name="undo-variant"
-            type="material-community"
-            color="#FDD0A9"
-            raised
-            onPress={this.handleButtonPress}
-            size={30}
-          />
-          <Icon
-            name="dislike1"
-            type="antdesign"
-            color="#F5A159"
-            raised
-            onPress={this.handleButtonPress}
-            size={40}
-          />
-          <Icon
-            name="like1"
-            type="antdesign"
-            color="#7ED1EF"
-            raised
-            onPress={this.handleButtonPress}
-            size={40}
-          />
-          <Icon
-            name="heart"
-            type="antdesign"
-            color="#FF8D8D"
-            raised
-            onPress={this.handleButtonPress}
-            size={30}
-          />
-        </View>
-      </View>
-    </View>
-  );
 
   renderNoMoreCards = () => (
     <Card>
@@ -95,7 +42,6 @@ class QuizScreen extends Component {
           onSwipe={this.handleResponse}
           data={questions}
           keyProp="id"
-          renderCard={this.renderCards}
           renderNoMoreCards={this.renderNoMoreCards}
         />
       </SafeAreaView>
@@ -107,42 +53,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#FFF8F3",
-  },
-  card: {
-    backgroundColor: "#FFF8F3",
-    width: "90%",
-    marginLeft: "auto",
-    marginRight: "auto",
-    marginTop: "10%",
-    height: "145%",
-    borderRadius: 30,
-  },
-  questionView: {
-    alignContent: "center",
-    backgroundColor: "#F9DAC4",
-    borderBottomLeftRadius: 30,
-    borderBottomRightRadius: 30,
-    borderTopLeftRadius: 15,
-    borderTopRightRadius: 15,
-  },
-  questionTitle: {
-    color: "#FBBA82",
-    backgroundColor: "#FFF8F3",
-    fontSize: 30,
-    fontWeight: "bold",
-    textAlign: "center",
-    marginTop: 30,
-    marginBottom: 30,
-  },
-  buttonsContainer: {
-    flex: 1,
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "center",
-    marginTop: -20,
-  },
-  buttonContainer: {
-    flex: 1,
   },
 });
 
