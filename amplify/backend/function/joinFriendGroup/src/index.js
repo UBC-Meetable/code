1ef04e03292387c288488c16d2c06c328adbffdd
alexzhou <azhou02@students.cs.ubc.ca>
2021-06-09 01:42:51 -0700

    lambda fixes

--- amplify/backend/function/joinFriendGroup/src/index.js
+++ amplify/backend/function/joinFriendGroup/src/index.js
@@ -4,8 +4,8 @@
 Amplify Params - DO NOT EDIT */
 
 const AWS = require('aws-sdk');
-import PriorityQueue from "priorityqueue";
-import { updateConstructorTypeNode } from "typescript";
+const PriorityQueue = require("priorityqueue");
+//import { updateConstructorTypeNode } from "typescript";
 const docClient = new AWS.DynamoDB.DocumentClient();
 
 const tables = {
@@ -20,16 +20,18 @@ exports.handler = async (event) => {
   const MIN_SCORE = 2; // consider passing with the lambda input
   const MAX_GROUP_SIZE = 10;
   const SCORE_IF_GROUPED_BEOFRE = 0; // could consider a score reduction instead
-  const pq = new PriorityQueue({ function(a, b) {a[1] == b[1] ? 0 : (a[1] > b[1] ? -1 : 1)} }); // maxPQ containing type [user, score]
+  const pq = new PriorityQueue.BinaryHeap({ function(a, b) {a[1] == b[1] ? 0 : (a[1] > b[1] ? -1 : 1)} }); // maxPQ containing type [user, score]
+  let groupToPut = null; // string groupID of group to join or null if none found
 
   // note: year is a reserved word in AWS DynamoDB, must use name substitution via ExpressionAttributeNames
   const bucketQueryParams = {
-    TableName: tables[user],
-    KeyConditionExpression: "university = :uni",
+    TableName: tables.user,
+    KeyConditionExpression: "university = :uni AND year = :year",
     ExpressionAttributeNames: {
     },
     ExpressionAttributeValues: {
       ":uni": incomingUser.university,
+      ":year": incomingUser.year,
     }
   };
 
@@ -45,7 +47,7 @@ exports.handler = async (event) => {
 
     await Promise.all(bucket.Items.map(async function(user, index, array) {
       const res =  await docClient.query({
-        TableName: tables[quiz],
+        TableName: tables.quiz,
         KeyConditionExpression: "userID = :userID",
         ExpressionAttributeValues: {
           ":userID": user.id // note: assumption is one quiz per user for now
@@ -116,14 +118,13 @@ exports.handler = async (event) => {
     // init maxPQ buildHeap
     // let pq = PriorityQueue.from(userScorePairs, { comparator: function(a, b) {a[1] == b[1] ? 0 : (a[1] > b[1] ? -1 : 1)} }); 
 
-    groupToPut = null;
     while (!pq.isEmpty()) {
       let [user, score] = pq.pop();
       if (score < MIN_SCORE) break;
       SmallestNonEmpty = {size: Infinity, id: ""};
       existingGroups[user.id].forEach(function(groupID) {
         docClient.get({
-          TableName: tables[friendGroup],
+          TableName: tables.friendGroup,
           Key: {"groupID": groupID},
           ConsistentRead: true
         }).promise.then(
@@ -146,7 +147,6 @@ exports.handler = async (event) => {
       console.log(error);
   }
   
-  // TODO implement
   const response = {
       statusCode: 200,
   //  Uncomment below to enable CORS requests
@@ -155,14 +155,13 @@ exports.handler = async (event) => {
   //      "Access-Control-Allow-Headers": "*"
   //  }, 
       body: JSON.stringify(groupToPut),
-      //body: JSON.stringify('Hello from Lambda!'),
   };
   return response;
 };
 
 async function fillGroupSet(userid, set) {
   docClient.query({
-    TableName: tables[friendGroupConnectionModel],
+    TableName: tables.friendGroupConnectionModel,
     KeyConditionExpression: "userID = :userid",
     ExpressionAttributeValues: {
       ":userid": userid
@@ -177,11 +176,6 @@ async function fillGroupSet(userid, set) {
   );
 }
 
-async function joinOwnGroup() {
-  
-
-}
-
 // [QAPair], [QAPair] -> int
 function compatibilityScore(q1, q2) {
   const user1Responses = new Map(q1.map(i => [i.q, i.a]));
@@ -193,30 +187,7 @@ function compatibilityScore(q1, q2) {
   return score;
 }
 
-// qi, qi1, qi2 are array of QApairs
-// function moreCompatible(qi, qi1, qi2) {
-//     const MIN_SCORE = 2;
-//     if (qi2 == null) {
-//       return true;
-//     }
-//     const arrToMap = (map, obj) => { map[obj.q] = obj.a; return map}
-//     const userResponses = qi.reduce(arrToMap, {});
-//     const user1Responses = qi1.reduce(arrToMap, {});
-//     const user2Responses = qi2.reduce(arrToMap, {});
-//     let user1Score = 0;
-//     let user2Score = 0;
-//     for (const [q, a] of userResponses) {
-//       if (a === user1Responses.get(q)) user1Score += 1;
-//       if (a === user2Responses.get(q)) user2Score += 1;
-//     }
-//     if (user1Score > user2Score && user1Score >= MIN_SCORE) return true;
-//     return false;
-//   }
-
-
 /*
-
-
 Fetch a subset of users from db based on some key e.g. {school, year}, could be done with db indexing
 Iterate over the subset of users and put them into a priority queue based on similarity score
 As above, reduce score if pair are in an existing group
@@ -226,5 +197,4 @@ join top user’s smallest non-full group if one exists
 Otherwise, pop top user and repeat
 If PQ is empty, create own group and join
 Save everything in DB
-
 */
