21fb373e78d08aee40bae818422629d33152e1bc
TrevorFlanigan <trevorflanigan@alumni.ubc.ca>
2021-08-31 12:40:52 -0700

    added picture icon to chat

--- src/components/Chat/MessageInput.tsx
+++ src/components/Chat/MessageInput.tsx
@@ -1,6 +1,8 @@
-import { Layout } from "@ui-kitten/components";
+import { Layout, Text } from "@ui-kitten/components";
 import React from "react";
-import { KeyboardAvoidingView, StyleSheet, TextInput } from "react-native";
+import { StyleSheet, TextInput } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { Badge, Icon } from "react-native-elements";
 import Colors from "../../constants/Colors";
 
 type MessageInputProps = {
@@ -14,44 +16,59 @@ const styles = StyleSheet.create({
   input: {
     minHeight: 60,
     borderRadius: 20,
+    flex: 0.9,
     padding: 10,
-    width: "90%",
     backgroundColor: Colors.theme.creme,
   },
   inputContainer: {
-    // width: "100%",
     backgroundColor: "#0000",
     justifyContent: "center",
     alignItems: "center",
-    paddingVertical: 10,
-    // flexBasis: 50,
-    // height: "%",
+    flexDirection: "row",
+  },
+  iconContainer: {
+    flex: 0.1,
+    minHeight: 60,
+    backgroundColor: "#0000",
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
 
 const MessageInput = ({
   value, onChangeText, onFocus, onSubmitEditing,
-}: MessageInputProps) => (
-  <Layout style={styles.inputContainer}>
-    <TextInput
-      value={value}
-      onChangeText={onChangeText}
-      style={styles.input}
-      onFocus={
-        onFocus
-      }
-      placeholder="Say Hi!"
-      placeholderTextColor="#c4c4c4"
-      textBreakStrategy="highQuality"
-      numberOfLines={1}
-      underlineColorAndroid="rgba(0,0,0,0)"
-      blurOnSubmit={false}
-      onSubmitEditing={
-        onSubmitEditing
-      }
-    />
-  </Layout>
-//   </KeyboardAvoidingView>
-);
+}: MessageInputProps) => {
+  const units = useSafeAreaInsets();
+  return (
+    <Layout style={[styles.inputContainer, {
+      paddingHorizontal: units.left + units.right,
+      paddingBottom: 10,
+    }]}
+    >
+      <Layout style={styles.iconContainer}>
+        <Icon name="photo" />
+      </Layout>
+
+      <TextInput
+        value={value}
+        onChangeText={onChangeText}
+        style={[styles.input, { marginHorizontal: units.left + units.right }]}
+        onFocus={
+          onFocus
+        }
+        placeholder="Say Hi!"
+        placeholderTextColor="#c4c4c4"
+        textBreakStrategy="highQuality"
+        numberOfLines={1}
+        underlineColorAndroid="rgba(0,0,0,0)"
+        blurOnSubmit={false}
+        onSubmitEditing={
+          onSubmitEditing
+        }
+      />
+    </Layout>
+  //   </KeyboardAvoidingView>
+  );
+};
 
 export default MessageInput;
