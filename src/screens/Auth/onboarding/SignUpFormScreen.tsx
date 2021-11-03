c7dfe417f5e39dad9c3fa17c48ffc40470fd4f52
Mayank Rastogi <mayankrustey2002@gmail.com>
2021-11-02 20:35:10 -0700

    Sign up errors debugged

--- src/screens/Auth/onboarding/SignUpFormScreen.tsx
+++ src/screens/Auth/onboarding/SignUpFormScreen.tsx
@@ -59,17 +59,7 @@ const SignUpFormScreen = ({ onLogIn, onCreate }: SignUpFormScreenProps) => {
     if (!password) {
       setError((prevErrors) => [...prevErrors, "Password cannot be blank"]);
       flag = false;
-    }
-    if(password.length < 8) {
-      setError((prevErrors) => [...prevErrors, "Password should be greater than 8 characters"]);
-      flag = false;
-    }
-    const expression = /(?!.*\.{2})^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([\t]*\r\n)?[\t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([\t]*\r\n)?[\t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
-    if (expression.test(email.toLowerCase()) === false) {
-      console.log("Email is Not Correct");
-      setError((prevErrors) => [...prevErrors, "Email format is not correct"]);
-      flag = false;
-    }
+    }  
     return flag;
   };
 
@@ -100,6 +90,7 @@ const SignUpFormScreen = ({ onLogIn, onCreate }: SignUpFormScreenProps) => {
       console.log(e);
 
       setError((prevErrors) => [...prevErrors, message]);
+      setErrorModal(true);
     } finally {
       setLoading(false);
     }
