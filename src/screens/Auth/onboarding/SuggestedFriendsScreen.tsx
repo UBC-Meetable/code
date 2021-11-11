9bf4a561802d0b0b9bf8f6e9af432639f8825d9a
steven wong <wong.steven604@gmail.com>
2021-11-10 20:36:22 -0800

    add updating find friend option functionality and updated user model schema to include setFrindFriendsOptionSelection field

--- src/screens/Auth/onboarding/SuggestedFriendsScreen.tsx
+++ src/screens/Auth/onboarding/SuggestedFriendsScreen.tsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Button, Layout, Text } from "@ui-kitten/components";
+import useUserProfile from "../../../hooks/useUserProfile";
+import updateUserProfile from "../../../calls/updateUserCourses";
 
 const SuggestedFriendsScreen = () => {
+  const [findFriendsOption, setFindFriendsOption] = useState<boolean>(false);
+  const { info: userProfile } = useUserProfile();
   return (
     <div>
       <h1>Would you like for us to help you find some new friends?</h1>
       <Button>Yes </Button>
       <Button>No </Button>
+      <Button
+        onPress={async () => {
+          await updateUserProfile({
+            id: userProfile!.id,
+            findFriendsOptionSelected: findFriendsOption,
+          });
+        }}
+      >
+        Done
+      </Button>
     </div>
   );
 };
