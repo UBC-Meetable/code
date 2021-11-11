58563808f97e5e06244ed159bc9cce4f2131b5cc
steven wong <wong.steven604@gmail.com>
2021-11-10 19:55:22 -0800

    build skeleton for SuggestedFriendsScreen screen

---
+++ src/screens/Auth/onboarding/SuggestedFriendsScreen.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Button, Layout, Text } from "@ui-kitten/components";
+
+const SuggestedFriendsScreen = () => {
+  return (
+    <div>
+      <h1>Would you like for us to help you find some new friends?</h1>
+      <Button>Yes </Button>
+      <Button>No </Button>
+    </div>
+  );
+};
+
+export default SuggestedFriendsScreen;
