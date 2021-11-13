aa926ae89260f16cfc5d5ec8f35cf5f1f5d2c8a8
steven wong <wong.steven604@gmail.com>
2021-11-12 22:11:38 -0800

    build skeleton of your interests screen for onboarding flow

---
+++ src/screens/Auth/onboarding/YourInterestsScreen.tsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import { StyleSheet } from "react-native";
+import { Layout, Text, Toggle } from "@ui-kitten/components";
+import useUserProfile from "../../../hooks/useUserProfile";
+import updateUserProfile from "../../../calls/updateUserCourses";
+
+const YourInterestsScreen = () => {
+  const [interests, setInterests] = useState<string[]>([]);
+  const { info: userProfile } = useUserProfile();
+
+  return (
+    <Layout style={styles.container}>
+      <Text>interests screen. will update this text</Text>
+    </Layout>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#0000",
+    marginTop: 250,
+    marginLeft: 30,
+  },
+});
+
+export default YourInterestsScreen;
