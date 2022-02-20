6035232cff87fcf0c2068144e9c1b5adf27a791a
Ali Salamatian <alisalamatian1@gmail.com>
2022-02-20 10:12:41 -0800

    the SuggestedFriend component; not working API

--- src/screens/SuggestedFriends.tsx
+++ src/screens/SuggestedFriends.tsx
@@ -1,16 +1,37 @@
 import { StackNavigationProp } from "@react-navigation/stack";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { RootStackParamList } from "../types";
 import { Text, StyleSheet } from "react-native";
+import {API} from "aws-amplify";
 
 
 
 
 const SuggestedFriends = ()=> {
+    const [suggestedFriends, setSuggestedFriends] = useState([]);
+    useEffect(()=> {
+    const fetchFriends = async ()=>{
+        console.log("We are inside the suggestedFriends Before calling API");
+        const friends = await API.get("friendSuggestionapi","/friendGetting", "")
+        setSuggestedFriends(friends);
+        console.log("We are inside the suggestedFriends after!!");
+        console.log(friends);
+    }
+    fetchFriends()
+        .catch(console.error)
+    }, []);
     const GetSuggestedFriends = ()=> {
         return(           
             <Text>
-                Hello this is the list of suggested friends:
+               {suggestedFriends.map((friend: any) => {   
+                  <Text>
+                    {console.log("---------------------")}
+                    {console.log(friend)}
+                    {console.log(friend.name)}
+                    {friend.name},
+                    {friend.education}
+                  </Text>                
+                })}
             </Text>
         )
     }
