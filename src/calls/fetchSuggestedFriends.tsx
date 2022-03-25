3000e0e5558bfec346427a02f74f5ab37a9b54c8
Savannah Feder <sfeder0@gmail.com>
2022-03-25 13:00:17 -0700

    started connecting frontend to backend

---
+++ src/calls/fetchSuggestedFriends.tsx
@@ -0,0 +1,9 @@
+import { GraphQLResult } from "@aws-amplify/api";
+import { API } from "aws-amplify";
+
+//listUsersQuery -> the lambda function they wrote
+const fetchSuggestedFriends = () => {
+  console.log("test");
+};
+
+export default fetchSuggestedFriends;
