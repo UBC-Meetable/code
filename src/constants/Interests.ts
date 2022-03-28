fae9a52f17895a89ccacf92eb3b80a9e03c0f7cf
Savannah Feder <sfeder0@gmail.com>
2022-03-27 17:55:27 -0700

    added more interests

--- src/constants/Interests.ts
+++ src/constants/Interests.ts
@@ -1,42 +1,109 @@
-export const interests = [
-    {
-      name: "reading",
-      category: "nerd stuff",
-      selected: false,
-    },
-  
-    {
-      name: "coding",
-      category: "nerd stuff",
-      selected: false,
-    },
-  
-    {
-      name: "anime",
-      category: "nerd stuff",
-      selected: false,
-    },
-  
-    {
-      name: "painting",
-      category: "nerd stuff",
-      selected: false,
-    },
-  
-    {
-      name: "swimming",
-      category: "outdoor activities",
-      selected: false,
-    },
-  
-    {
-      name: "hiking",
-      category: "outdoor activities",
-      selected: false,
-    },
-    {
-      name: "rock climbing",
-      category: "outdoor activities",
-      selected: false,
-    },
-  ];
+const interests = [
+  {
+    name: "reading📚",
+    category: "nerd stuff",
+    selected: false,
+  },
+  {
+    name: "coding👩‍💻",
+    category: "nerd stuff",
+    selected: false,
+  },
+  {
+    name: "anime📺",
+    category: "nerd stuff",
+    selected: false,
+  },
+  {
+    name: "painting🎨",
+    category: "nerd stuff",
+    selected: false,
+  },
+  {
+    name: "swimming🏊‍♀️",
+    category: "sports",
+    selected: false,
+  },
+  {
+    name: "hiking⛰️",
+    category: "sports",
+    selected: false,
+  },
+  {
+    name: "basketball🏀",
+    category: "sports",
+    selected: false,
+  },
+  {
+    name: "fitness🏃‍♀️",
+    category: "sports",
+    selected: false,
+  },
+  {
+    name: "walking🚶",
+    category: "sports",
+    selected: false,
+  },
+  {
+    name: "soccer⚽️",
+    category: "sports",
+    selected: false,
+  },
+  {
+    name: "yoga🧘‍♀️",
+    category: "sports",
+    selected: false,
+  },
+  {
+    name: "dancing🕺",
+    category: "sports",
+    selected: false,
+  },
+  {
+    name: "cycling🚴‍♀️",
+    category: "sports",
+    selected: false,
+  },
+  {
+    name: "photography📸",
+    category: "hobbies",
+    selected: false,
+  },
+  {
+    name: "journalling✍️",
+    category: "hobbies",
+    selected: false,
+  },
+  {
+    name: "tech🧑‍💻",
+    category: "nerd stuff",
+    selected: false,
+  },
+  {
+    name: "gaming🎮",
+    category: "nerd stuff",
+    selected: false,
+  },
+  {
+    name: "movies📺",
+    category: "liesure activities",
+    selected: false,
+  },
+  {
+    name: "tv📺",
+    category: "liesure activities",
+    selected: false,
+  },
+  {
+    name: "comedy🤣",
+    category: "liesure activities",
+    selected: false,
+  },
+  {
+    name: "fantasy🧙",
+    category: "nerdy stuff",
+    selected: false,
+  },
+];
+
+export default interests;
