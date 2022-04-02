b7e90ce4c02b90c70e38853d3396130b120cdfa9
Mayank Rastogi <mayankrustey2002@gmail.com>
2022-04-02 14:38:34 -0700

    Added Edit Interest Page

--- src/screens/EditInterests.tsx
+++ src/screens/EditInterests.tsx
@@ -17,6 +17,8 @@ import PrimaryButton from "../components/ui/PrimaryButton";
 import GradientButton from "../components/ui/GradientButton";
 import { StackNavigationProp } from "@react-navigation/stack";
 import { RootStackParamList } from "../types";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { FlexViewCrossStyleProps } from "@ui-kitten/components/devsupport";
 // import { Modalize } from 'react-native-modalize';
 /** TODO: Cache user profile so we don't need to fetch so often. */
 
@@ -27,270 +29,180 @@ const Label = ({ title }: LabelProps) => <Text style={styles.ModalField}>{title}
 
 const EditInterestsPage =
   ({ navigation }:{ navigation: StackNavigationProp<RootStackParamList, "EditInterest">;}) => {
-    // navigation.push()
 
-  // const headerHeight = useHeaderHeight();
-
-  // const { loading, set, ...user } = useUserProfile();
-  // const [bio, setBio] = React.useState(user.bio);
-  // const [firstName, setFirstName] = React.useState(user.firstName);
-  // const [lastName, setLastName] = React.useState(user.lastName);
-  // const [key, setKey] = React.useState(user.profilePicture);
-  // const [visible, setVisible] = React.useState(false);
-
-  // const pickImage = async () => {
-  //   const permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
-
-  //   if (permissionResult.granted === false) {
-  //     console.warn("Permission to access camera roll is required!");
-  //     return;
-  //   }
-
-  //   const result = await ImagePicker.launchImageLibraryAsync({
-  //     mediaTypes: ImagePicker.MediaTypeOptions.Images,
-  //     allowsEditing: true,
-  //     aspect: [1, 1],
-  //     quality: 0.5,
-  //   });
-
-  //   if (!result.cancelled) {
-  //     const cropped = await ImageManipulator.manipulateAsync(result.uri, [{
-  //       resize: {
-  //         height: 125,
-  //         width: 125,
-  //       },
-  //     }]);
-  //     uploadImage(cropped);
-  //   }
-  // };
-
-  // const uploadImage = (toUpload: ImageInfo) => {
-  //   const imageName = toUpload.uri.replace(/^.*[\\/]/, "");
-  //   const imageKey = `${user.id}/${imageName}`;
-  //   console.log(toUpload.uri);
-
-  //   fetch(toUpload.uri).then((response) => {
-  //     response.blob()
-  //       .then((blob) => {
-  //         const access = { level: "public", contentType: toUpload.type };
-  //         Storage.put(imageKey, blob, access)
-  //           .then(() => {
-  //             Storage.get(imageKey, { download: false, expires: 604800 }).then(() => {
-  //               updateImageKey(imageKey);
-  //               setKey(imageKey);
-  //             });
-  //           });
-  //       });
-  //   });
-  // };
-
-  // /** TODO Cache Profile Images */
-  // const updateImageKey = (imageKey: string) => {
-  //   set({ id: user.id, profilePicture: imageKey });
-  // };
-
-  // if (loading) {
-  //   return (
-  //     <ScrollView
-  //       contentContainerStyle={[profileStyles.container, { paddingTop: headerHeight }]}
-  //       bounces={false}
-  //     >
-  //       <Spinner />
-  //     </ScrollView>
-  //   );
-  // }
-
-  // const open = () => setVisible(true);
-  // const close = () => {
-  //   reset();
-  //   setVisible(false);
-  // };
-  // const save = () => {
-  //   set({
-  //     id: user.id,
-  //     email: user.email,
-  //     bio,
-  //     firstName,
-  //     lastName,
-  //   });
-  //   setVisible(false);
-  // };
-  // const reset = () => {
-  //   setBio(user.bio);
-  //   setFirstName(user.firstName);
-  //   setLastName(user.lastName);
-  // };
+  
 
   return (
-    <Text
-        style={styles.name}
+    <SafeAreaView style={styles.root}>
+      <Layout style={styles.container}>
+        <ScrollView contentContainerStyle={styles.selectionsContainer} />
+          <Text style={styles.title}>
+            Edit Interests
+          </Text>
+          <Text style={styles.subtitle}>
+            Interest Category
+          </Text>
+          <View
+        style={styles.itemContainer}
       >
-        Bio
-    </Text>
-    // <ScrollView
-    //   contentContainerStyle={[
-    //     profileStyles.container,
-    //     { paddingTop: headerHeight },
-    //   ]}
-    //   bounces={false}
-    // >
-    //   <ProfilePicture imageKey={key} style={styles.image}/>
-    //   <Text style={styles.name}>
-    //     {`${firstName} ${lastName}`.trim()}
-    //   </Text>
-    //   <Text style={styles.email}>
-    //     {user.email || ""}
-    //   </Text>
-    //   <GradientButton
-    //     onPress={open}
-    //     style={styles.editButton}
-    //     status="info"
-    //   >
-    //     Edit Profile
-    //   </GradientButton>
-    //   <Text
-    //     style={styles.name}
-    //   >
-    //     Bio
-    //   </Text>
-    //   <View
-    //     style={styles.itemContainer}
-    //   >
-    //     <Text
-    //       style={styles.bio}
-    //     >
-    //       {bio}
-    //     </Text>
-    //   </View>
-    //   <Text
-    //     style={styles.name}
-    //   >
-    //     Enrolled Classes
-    //   </Text>
-    //   <View
-    //     style={styles.itemContainer}
-    //   >
-    //     <View
-    //       style={styles.textDiv}
-    //     >
-    //       <Text>
-    //         CPEN211
-    //       </Text>
-    //     </View>
-    //     <View
-    //       style={styles.textDiv}
-    //       >
-    //       <Text>
-    //         MATH256
-    //       </Text>
-    //     </View>
-    //     <View
-    //       style={styles.textDiv}
-    //       >
-    //       <Text>
-    //         CPEN221
-    //       </Text>
-    //     </View>
-    //   </View>
-    //   <Text
-    //     style={styles.name}
-    //   >
-    //     Interests
-    //   </Text>
-    //   <View
-    //     style={styles.itemContainer}
-    //   >
-    //     <View
-    //       style={styles.textDiv}
-    //     >
-    //       <Text>
-    //         Anime 📺
-    //       </Text>
-    //     </View>
-    //     <View
-    //       style={styles.textDiv}
-    //       >
-    //       <Text>
-    //         Gaming 🎮
-    //       </Text>
-    //     </View>
-    //     <View
-    //       style={styles.textDiv}
-    //       >
-    //       <Text>
-    //         Formula 1 🏎️
-    //       </Text>
-    //     </View>
-    //   </View>
-    //   <Modal
-    //     animationType="slide"
-    //     presentationStyle="formSheet"
-    //     swipeToClose={true}
-    //     propagateSwipe={true}
-    //     visible={visible}
-    //   >
-    //     <KeyboardAvoidingView
-    //   // style={styles.container}
-    //   behavior="position"
-    //   style={{
-    //     padding:10,
-    //     backgroundColor: Colors.theme.lightCreme,
-    //   }}
-    // >
-    //     <ScrollView style={styles.MainModal}>
-    //     <Layout style={styles.modal}>
-    //       <Text style={styles.title}>Edit Profile</Text>
-    //       <Layout
-    //         style={styles.photoContainer}
-    //       >
-    //         <ProfilePicture
-    //           imageKey={key}
-    //           onPress={pickImage}
-    //         />
-    //       </Layout>
-    //       <Input
-    //         label={() => <Label title="First Name" />}
-    //         value={firstName || ""}
-    //         placeholder="First Name"
-    //         onChangeText={setFirstName}
-    //         style={styles.inputStyle}
-    //         textStyle={profileStyles.inputTextStyle}
-    //       />
-    //       <Input
-    //         label={() => <Label title="Last Name" />}
-    //         value={lastName || ""}
-    //         placeholder="Last Name"
-    //         onChangeText={setLastName}
-    //         style={styles.inputStyle}
-    //         textStyle={profileStyles.inputTextStyle}
-    //       />
-    //       <Input
-    //         label={() => <Label title="Bio" />}
-    //         value={bio || ""}
-    //         placeholder="Write a short bio about yourself..."
-    //         multiline
-    //         scrollEnabled
-    //         numberOfLines={1}
-    //         maxLength={175}
-    //         onChangeText={setBio}
-    //         style={styles.inputStyle}
-    //         textStyle={profileStyles.inputTextStyle}
-    //       />
-    //       <GradientButton onPress={save} style={
-    //         { marginTop: 16, marginBottom: 16, alignSelf: "center", width: "100%", height: 60 }
-    //         }>Save</GradientButton>
-    //       <GradientButton appearance="ghost" style={
-    //         { alignSelf: "center", width: "100%", height: 60 }
-    //         } onPress={close}>Close</GradientButton>
-    //     </Layout>
-    //     </ScrollView>
-    //     </KeyboardAvoidingView>
-    //   </Modal>
-    // </ScrollView>
+        <View
+          style={styles.textDiv}
+        >
+          <Text>
+            Anime 📺
+          </Text>
+        </View>
+        <View
+          style={styles.textDiv}
+          >
+          <Text>
+            Gaming 🎮
+          </Text>
+        </View>
+        <View
+          style={styles.textDiv}
+          >
+          <Text>
+            Formula 1 🏎️
+          </Text>
+        </View>        
+              
+      </View>
+      <View
+        style={styles.itemContainer}
+      >
+        <View
+          style={styles.textDiv}
+        >
+          <Text>
+            Anime 📺
+          </Text>
+        </View>
+        <View
+          style={styles.textDiv}
+          >
+          <Text>
+            Gaming 🎮
+          </Text>
+        </View>
+        <View
+          style={styles.textDiv}
+          >
+          <Text>
+            Formula 1 🏎️
+          </Text>
+        </View>        
+              
+      </View>
+      <View style={{height: 50}}>
+
+      </View>
+
+          <Text style={styles.subtitle}>
+            Interest Category
+          </Text>
+          <View
+        style={styles.itemContainer}
+      >
+        <View
+          style={styles.textDiv}
+        >
+          <Text>
+            Anime 📺
+          </Text>
+        </View>
+        <View
+          style={styles.textDiv}
+          >
+          <Text>
+            Gaming 🎮
+          </Text>
+        </View>
+        <View
+          style={styles.textDiv}
+          >
+          <Text>
+            Formula 1 🏎️
+          </Text>
+        </View>        
+      </View>
+      <View
+        style={styles.itemContainer}
+      >
+        <View
+          style={styles.textDiv}
+        >
+          <Text>
+            Anime 📺
+          </Text>
+        </View>
+        <View
+          style={styles.textDiv}
+          >
+          <Text>
+            Gaming 🎮
+          </Text>
+        </View>
+        <View
+          style={styles.textDiv}
+          >
+          <Text>
+            Formula 1 🏎️
+          </Text>
+        </View>        
+      </View>
+      
+          <GradientButton
+          status="info"
+          style={styles.editButton}
+          //TODO: Change this to Edit Interests Page
+          onPress={() => {
+            // navigation.push("FriendOptions");
+            navigation.pop();
+
+            // alert('You\'re gonna edit your Interests!');
+          }}
+        >
+          Close
+        </GradientButton>
+      </Layout>
+  </SafeAreaView>
   );
 };
 
+
 const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    flexDirection: "column",
+    display: "flex",
+    height: "100%",
+    // justifyContent: "space-between",
+    // alignItems: "center",
+    backgroundColor: Colors.theme.lightCreme,
+  },
+  container: {
+    width: "100%",
+    // height: "100%",
+    marginTop: 10,
+    marginLeft: 10,
+    marginRight: 10,
+    marginBottom: 10,
+    alignItems: "center",
+    backgroundColor: Colors.theme.lightCreme,
+  },
+  selectionsContainer: {
+    display: "flex",
+    flexDirection: "column",
+    // justifyContent: "space-between",
+    // alignItems: "center",
+    width: "75%",
+    backgroundColor: Colors.theme.lightCreme,
+    marginTop: 10,
+    marginBottom: 10,
+    marginLeft: 10,
+    marginRight: 10,
+  },
   name: {
     fontFamily: "Quicksand_600SemiBold",
     color: "#000000",
@@ -309,6 +221,9 @@ const styles = StyleSheet.create({
     marginVertical: 12,
     width: "90%",
     height: 60,
+    // position: "absolute",
+    // bottom: "0%",
+    // width:"100%",
   },
   email: { fontSize: 14 },
   image: {
@@ -325,7 +240,16 @@ const styles = StyleSheet.create({
   },
   title: {
     fontFamily: "Quicksand_600SemiBold",
-    fontSize: 20,
+    fontSize: 24,
+    fontWeight: "900",
+    color: "#000000",
+    textAlign: "center",
+    marginTop: 16,
+    marginBottom: 20
+  },
+  subtitle: {
+    fontFamily: "Quicksand_600SemiBold",
+    fontSize: 18,
     color: "#000000",
     textAlign: "center",
     marginTop: 16,
@@ -358,7 +282,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#FFEAB4',
     borderRadius: 20,
     padding:10,
-    margin: 10
+    margin: 10,
   },
   itemContainer: {
     width: "90%",
@@ -368,6 +292,7 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignContent: "center",
     alignItems: "center",
+    display: "flex"
   },
   photoContainer: {
     backgroundColor: "#0000",
@@ -380,6 +305,10 @@ const styles = StyleSheet.create({
     marginVertical: 5,
     borderRadius: 5,
   },
+  bottom: {
+    flex: 1,
+    justifyContent: "flex-end",
+  }
 });
 
 export default EditInterestsPage;
